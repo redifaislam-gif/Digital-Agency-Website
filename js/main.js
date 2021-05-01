4c9248fdf0e0d6786e74cb2bc878a67e8ef103de
Redifa Islam <73694367+redifaislam-gif@users.noreply.github.com>
2021-05-01 23:09:05 +0600

    t
    t

--- js/main.js
+++ js/main.js
@@ -1,21 +1,63 @@
-
+$(document).ready(function(){
 	
 	
-
-
+//stikey
+	$(".js--clint-section").waypoint(function(direction){
+		
+	if(direction== "down"){
+		$(".menu-section").addClass("stikey");
+	}else{
+	$(".menu-section").removeClass("stikey");	
+	}	
+		
+	});
+	//protfolio
+	var mixer=mixitup('.container');
+	
+	
+	$(".hellow-circle").circleProgress();	
+		
 	
 
- function openNav() {
+$(".hellow-circle").circleProgress();	
+	
+	
+	
+	
+	
+$('.owl-carosel').owlCarousel();	
+	
+});
+
+
+
+
+$(document).ready(function(){
+	
+	$(".mobile").click(function(){
+		
+		$(".menu-bar").slideToggle();
+		
+	});
+	
+	
+});
+	
+
+ function openNav5() {
 	  
-	  document.getElementById("myNav").style.width="100%";
+	  document.getElementById("myNav").style.width="30%";
   }
   
   
-  function closeNav() {
+  function closeNav5() {
 	  
 	  document.getElementById("myNav").style.width="0%";
   }
   
+
+
+	
   
 
  
